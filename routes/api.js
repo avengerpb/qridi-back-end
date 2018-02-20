791b17225c7a92685fda8b704aba025911b56e42
Danny Le <dannyle@Danny-Les-Macbook.local>
2018-02-20 15:05:48 +0200

    fix bug

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,13 @@ var options = {
   mode: 'text',
   pythonPath: 'python3',
   pythonOptions: ['-u'], // get print results in real-time
-  scriptPath: 'python'
+  scriptPath: 'polar_python'
 };
 
 router.get('/', function(req, res, next) {
-  PythonShell.run('authorization.py', options);
+  PythonShell.run('authorization.py', options, function(err){
+    if (err) throw err;
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,12 @@ var fetch = require('node-fetch');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  var data = readYaml.sync('python/config.yml');
+  var data = readYaml.sync('polar_python/config.yml');
   var id = data.user_id;
-  const request = require('node-fetch');
-  const inputBody = {"member-id": id};
+  var token  = data.access_token;
   const headers = {
-    'Authorization' : 'Bearer ' + id,
-    'Accept':'application/json'
+    'Accept':'application/json',
+    'Authorization' : 'Bearer ' + token
   };
 
     fetch('https://www.polaraccesslink.com/v3/users/'+id,
