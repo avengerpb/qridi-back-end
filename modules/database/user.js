da2e419d945f1bf2d4867651259ef8b6907a13e4
Danny Le <dannyle@Danny-Les-Macbook.local>
2018-03-16 22:35:41 +0200

    register

--- modules/database/user.js
+++ modules/database/user.js
@@ -8,4 +8,29 @@ function upsert(data){
     {data},{data},{upsert: true});
 }
 
+function register(newUser){
+  db.collection('Users').findOne({
+		$or: [
+			{'username': newUser.username},
+			{'email': newUser.email}
+		]
+	}, (err, user) => {
+		if(err) throw err;
+		if(!user) {
+			db.User.insert(newUser, (err, user) => {
+				if(err) { throw err; }
+				console.log('Registration succeed!');
+			});
+		} else {
+			if(user.username == newUser.username) {
+				console.log('This username has already existed!');
+			}
+			if(user.email == newUser.email){
+				console.log('This email has already existed!');
+			}
+		}
+		});
+}
+
 module.exports.upsert = upsert;
+module.exports.register = register;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@
 var express = require('express');
 var router = express.Router();
 var readYaml = require('read-yaml');
+var bcrypt = require('bcryptjs');
 const api = require('../modules/api/user')
+const data = require('../modules/database/user')
+
 
 //Student
 /* GET and store student_info from API */
@@ -13,6 +16,25 @@ router.get('/student_info', function(req, res, next) {
   var id = data.user_id;
   var token  = data.access_token;
   api.getInfo(id,token); //Call function to store user info to database
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', (req, res) => {
+	// let bcrypt = require('bcryptjs');
+	let salt = bcrypt.genSaltSync(10);
+	let hash = bcrypt.hashSync(req.body.password, salt);
+
+	let newUser = {
+		fullname: req.body.fullname,
+		username: req.body.username,
+		email: req.body.email,
+		password: hash,
+		user_type: 'Normal'
+	}
+	data.register(req.body.username,req.body.email)
 });
 
 //Teacher
